4f8ed14c80f98b010a45db58179a133e5491f71e
MV <15172121+m01001101-01010110@users.noreply.github.com>
2025-06-21 16:34:13 +0200

    jasmine_sprout: Setup Retrofit dynamic partitions

--- device.mk
+++ device.mk
@@ -21,10 +21,7 @@ AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS +=
     boot \
     system \
-    vendor \
-    product \
-    system_ext \
-    odm
+    vendor
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
