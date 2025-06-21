096551dfbdee923cf787f426a34c7649c6d08db4
MV <15172121+m01001101-01010110@users.noreply.github.com>
2025-06-21 22:15:06 +0200

    jasmine_sprout: Setup Retrofit dynamic partitions
    FAILED:
    build/make/core/Makefile:72: error: overriding commands for target `out/target/product/jasmine_sprout/recovery/root/first_stage_ramdisk/fstab.qcom', previously defined at build/make/core/base_rules.mk:533

--- device.mk
+++ device.mk
@@ -55,7 +55,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
 PRODUCT_COPY_FILES += \
-    device/xiaomi/jasmine_sprout/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
     device/xiaomi/jasmine_sprout/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
 # Init
