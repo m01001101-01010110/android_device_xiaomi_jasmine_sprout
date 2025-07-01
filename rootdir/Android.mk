39bef3f60da2b226f21a6630d6bb015e8147c28a
MV <15172121+m01001101-01010110@users.noreply.github.com>
2025-07-01 23:38:20 +0200

    Revert "jasmine_sprout: Drop verity"
    This reverts commit b5e161eeda8a049db6b5b75bbf8d45a6dc931ae3.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,3 +19,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.verity.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.verity.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
