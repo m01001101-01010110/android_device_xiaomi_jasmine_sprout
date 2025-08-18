f459d9c263dcacebb327d60f467eb6221d0dd0a4
MV <15172121+m01001101-01010110@users.noreply.github.com>
2025-08-18 11:58:21 +0200

    extract-utils: allow sha1sum optimization -lineage-21.0

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,9 +9,19 @@
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/libgf_ca.so)
+        [ "$2" = "" ] && return 0
             sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
